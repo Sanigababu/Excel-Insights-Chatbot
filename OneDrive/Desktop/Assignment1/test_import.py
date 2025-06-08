d795e5a522269ab8a531ea3002da97a380107fb2
Sanigababu <163140779+Sanigababu@users.noreply.github.com>
2025-06-09 04:39:33 +0530

    Delete OneDrive/Desktop/Assignment1 directory

--- OneDrive/Desktop/Assignment1/test_import.py
+++
@@ -1,6 +0,0 @@
-# test_import.py
-try:
-    from ast_parser import parse_rule, combine_rules, evaluate_rule, Node
-    print("Import successful!")
-except ImportError as e:
-    print(f"Import error: {e}")
